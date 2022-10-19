a3586e9b2e1b9c31b6c22ae437ac893b080792c8
ispan <ispan@DESKTOP-PAKSETB>
2022-10-19 17:28:35 +0800

    加入專案檔案。

---
+++ [(Exec_DateTime_Q1)-21 謝孟勳/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace __Exec_DateTime_Q1__21_謝孟勳
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("DVD借閱時間 : ");
+            string input = Console.ReadLine();
+            int input1 = int.Parse(input);
+            
+            DateTime dt = DateTime.Now.AddDays(input1);
+
+            string threeDaysAfter = dt.ToString("yyyy/MM/dd");
+            Console.WriteLine(threeDaysAfter);
+
+            Console.Read();
+        }
+    }
+}
